8d981ebcf5121d1a6505f335f1c38af5c51cfbc6
Josh Coles <josh@joshcoles.com>
2017-10-21 13:30:04 -0700

    Pulled skills through.

--- src/components/profile-components/Skills.jsx
+++ src/components/profile-components/Skills.jsx
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
 
 class Skills extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
   render() {
+
+    const skills = this.props.skills ? this.props.skills : null;
+
     return (
       <div className="skills profile-component">
         <h1>Skills</h1>
+        <ul>
+          {
+            skills && skills.map((data, index) => {
+              return (
+                <li className={data.category.toLowerCase()} key={index}>{data.name}
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
     );
   }

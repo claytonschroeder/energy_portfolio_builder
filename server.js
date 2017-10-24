b67fd1a1c7f770b8e4f88ca6dbb2e3f9f6506908
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2017-10-24 23:35:48 +0000

    change port

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000,
-      ignored: /node_modules/
-    }
-  })
-  .listen(3000, '0.0.0.0', function (err, result) {
-    if (err) {
-      console.log(err);
-    }
+const HOSTNAME = '0.0.0.0';
+const PORT = process.env.DEV_SERVER_PORT || 8000;
+const PROXY_PORT = process.env.PORT || 8001;
 
-    console.log('Running at http://0.0.0.0:3000');
-  });
+/* Since we are using the webpack dev server we need to add this as an entry */
+config.entry.unshift('webpack-dev-server/client?http://localhost:' + PORT);
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+  }
+})
+.listen(PORT, HOSTNAME, (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(`Running at http://${HOSTNAME}:${PORT}`);
+});

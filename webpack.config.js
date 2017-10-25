a3219552bce5a90372b317e43869b8b508559f0d
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2017-10-25 23:49:55 +0000

    source map for debug, add bootstrap.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'source-map',
   entry: [
     'es6-promise',
     './src/index.jsx'
